5de21377e61f5eabfcf8da63ab01d553d239feab
NikhilPeri <nikhil.peri@shopify.com>
2018-03-08 09:44:21 -0500

    database-object

--- migrations/001_create_proline_games_table.py
+++ migrations/001_create_proline_games_table.py
@@ -8,8 +8,8 @@ db = create_engine(os.environ['DATABASE_URL'])
 meta = MetaData(db)
 proline_games_table = Table('proline_games', meta,
                         Column('id',             Integer, primary_key=True),
-                        Column('created_at',      Time, nullable=False, default=datetime.utcnow),
-                        Column('updated_at',      Time, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow),
+                        Column('created_at',     Time, nullable=False, default=datetime.utcnow),
+                        Column('updated_at',     Time, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow),
                         Column('ticket_id',      Integer),
                         Column('game_id',        Integer),
                         Column('cutoff',         Time),
