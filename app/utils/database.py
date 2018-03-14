777a3b5c391e19edeac4535db9f81b7b9fb4786e
NikhilPeri <nikhil.peri@shopify.com>
2018-03-13 20:51:19 -0400

    migrations

--- app/utils/database.py
+++ app/utils/database.py
@@ -5,14 +5,17 @@ import os
 
 class Database(object):
     def __init__(self, env=None):
-        self.env = os.environ['APP_ENV'] if env == None else env
+        if env == None and 'APP_ENV' in os.environ:
+            self.env = os.environ['APP_ENV']
+        else:
+            self.env = env
 
         if self.env == 'PRODUCTION':
             self.db = psycopg2.connect(os.environ['DATABASE_URL'])
         elif self.env == 'DEVELOPMENT':
-            self.db = sqlite3.connect('data/development_db.sqlite')
+            self.db = sqlite3.connect('../../data/development_db.sqlite')
         elif self.env == 'TEST':
-            self.db = sqlite3.connect('data/test_db.sqlite')
+            self.db = sqlite3.connect('../../data/test_db.sqlite')
         else:
             raise "Invalid APP_ENV:" + self.env
 
