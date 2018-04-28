2d93187a06a65a33a1e0b6e1e5bd94d2e3b75743
NikhilPeri <nikhil.peri@shopify.com>
2018-04-28 17:02:29 -0400

    declaritive orm

---
+++ app/datasets/proline/tickets.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Index, Integer, Time, String, Float
+
+from datetime import datetime
+
+Base = declarative_base()
+
+class ProlineTickets(Base):
+    __tablename__ = 'proline_tickets'
+
+    id         = Column(Integer, primary_key=True)
+    created_at = Column(Time,    default=datetime.utcnow)
+    updated_at = Column(Time,    default=datetime.utcnow, onupdate=datetime.utcnow)
+    handle     = Column(Integer, nullable=False)
+    date       = Column(Date,    nullable=False)
+
+    idx_handle_date = Index(handle, date, unique=True)
