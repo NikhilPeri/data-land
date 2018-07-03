9f1ef94fb08de4a561b9bd4336ad2b9ef4bbdd36
NikhilPeri <nikhil.peri@shopify.com>
2018-07-03 08:55:10 -0400

    initial dataset class

---
+++ dataland/dataset.py
@@ -0,0 +1,15 @@
+class IncrementalDataset(object):
+    def __init__(self, directory):
+        self.directory = directory()
+        self.metadata
+
+    def load(self, start=None, end=None):
+
+    def batch_load(self, start=None, end=None):
+
+
+    def dump(self, dataframe, ):
+        # write a file with datframe csv
+
+class FullDataset(object):
+    def
