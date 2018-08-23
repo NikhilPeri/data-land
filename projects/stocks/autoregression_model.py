abe9b567d6a7795aeff1c0e561d5cd506bd2f260
NikhilPeri <nikhil.peri@shopify.com>
2018-08-23 19:44:08 -0400

    wip

--- projects/stocks/autoregression_model.py
+++ projects/stocks/autoregression_model.py
@@ -1,13 +1,22 @@
 import pandas as pd
 
-
+WINDOW_SIZE=30
 
 class BuildTrainingSet(TransformOperation):
+
     INPUT = 'data/stocks/snp_500_list.csv'
     OUTPUT = 'data/stocks/autoregression_model/train.csv'
 
+    NUMBER_OF_STOCKS_TO_SAMPLE = 30
+
     def transform(self, stock_list):
-        
+        stock_list = stock_list.sample(NUMBER_OF_STOCKS_TO_SAMPLE)
+        train_set = pd.DataFrame(columns=[''])
+        for index, company in stock_list.iterrows():
+            prices = pd.read_csv('data/stocks/prices/{}.csv'.format(company['Symbol']))
+
+    def build_windowd_set
+
     series = Series.from_csv('daily-minimum-temperatures.csv', header=0)
     values = DataFrame(series.values)
     dataframe = concat([values.shift(1), values], axis=1)
