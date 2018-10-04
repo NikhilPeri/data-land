3d9415707b11f22debeeb4c8f19a25b793058dae
NikhilPeri <nikhil.peri96@gmail.com>
2018-10-04 12:46:31 -0400

    Add storage object

--- dataland/storage.py
+++ dataland/storage.py
@@ -1,4 +1,8 @@
 import os
+import re
+import hashlib
+import base64
+
 from dataland import config
 from datetime import datetime
 from google.cloud import storage
@@ -11,27 +15,70 @@ def validate_path(path):
 def timestamp():
     return datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
 
+class StorageContext(object):
+     def __init__(self, storage, filepath, mode):
+         self.storage = storage
+         self.filepath = filepath
+         self.mode = mode
+
+    def __enter__(self):
+        self.storage.pull(filepath)
+        self.file = open(self.storage.local_path(filepath), mode=self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if exc_value is None and re.match(r'[wa]', self.mode) is not None:
+            self.file.close()
+            self.storage.push(filepath)
+        return True
+
 class Storage(object):
     def __init__(self):
         if not os.environ.has_key('GOOGLE_APPLICATION_CREDENTIALS'):
             os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = config['storage']['gcloud_credentials']
         self.bucket = storage.Client().bucket(config['storage']['gcloud_bucket'])
-        self.local_dir = config['storage']['local_dir']
+        self.local_dir = validate_path(config['storage']['local_dir'])
+        self.local_dir += '/' if not self.local_dir.endswith('/') else ''
+
+    def open(self, filepath, mode='r'):
+        return StorageContext(self, filepath, mode)
+
+    def pull(self, path):
+        for blob in self.list_blobs(path):
+            local_path = self.local_path(blob.name)
+            try:
+                with open(local_path, mode='r') as file:
+                    hash = base64.b64encode(hashlib.md5(file.read()).digest())
+                    if hash != blob.md5_hash:
+                        raise Exception('local cache invalid')
+            except:
+                try:
+                    os.makedirs(os.path.dirname(local_path))
+                except OSError as e:
+                    pass
+                blob.download_to_filename(local_path)
+
+    def push(self, path):
+        local_path = self.local_path(path)
+
+        if os.path.isfile(local_path):
+            self.bucket.blob(path).upload_from_filename(local_path)
+            return
+
+        for parent_dir, _, files in os.walk(local_path):
+            parent_dir = parent_dir.replace(self.local_dir, '')
+            for file in files:
+                filename = os.path.join(parent_dir, file)
+                import pdb; pdb.set_trace()
+                blob = self.bucket.blob(filename)
+                blob.upload_from_filename(self.local_path(filename))
 
     def list(self, path):
-        import pdb; pdb.set_trace()
+        blobs = list(self.list_blobs(path))
+        return { re.search('{}/[^/]+'.format(path), b.name).group() for b in blobs }
 
-    def fetch(self, path):
-        if os.path.exists(self.local_path(path)):
-            # Check MD5
-            # Check remote MD5
-            return True
-        else:
-            # Download remote
-            return False
+    def list_blobs(self, path=''):
+        return self.bucket.list_blobs(prefix=path)
 
     def local_path(self, path):
         return os.path.join(self.local_dir, validate_path(path))
-
-    def remote_path(self, path):
-        return "gs://{}".format(os.path.join(self.bucket.name, validate_path(path)))
