a9d00643a125c6991d14183e90882a1ae340b526
Nikhil <nperi104@uottawa.ca>
2018-12-18 18:38:13 -0500

    fix notification config

--- dataland/__init__.py
+++ dataland/__init__.py
@@ -1,7 +1,7 @@
 import yaml
 
 CONFIG_FILE = 'config/dataland.yml'
-
+NOTIFICATION_CONFIG = 'config/notifications.yml'
 global config
 with open(CONFIG_FILE, 'r') as config_file:
     config = yaml.load(config_file)
